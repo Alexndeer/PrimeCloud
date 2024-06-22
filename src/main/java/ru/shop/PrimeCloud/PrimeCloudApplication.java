7080ad1ac832ee5b5d600b183e2809a57cde345a
Alexndeer <Alexandroff007@gmail.com>
2024-06-22 21:04:01 +0300

    aboba

---
+++ src/main/java/ru/shop/PrimeCloud/PrimeCloudApplication.java
@@ -0,0 +1,13 @@
+package ru.shop.PrimeCloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeCloudApplication.class, args);
+	}
+
+}
