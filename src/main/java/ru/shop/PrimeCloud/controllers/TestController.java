4d6f28efafcc475e19cbe928f2250cc9529025c0
prodocc <tobi75500@gmail.com>
2024-07-09 14:15:38 +0300

    КТО ПРОЧЁЛ ТОТ ЗДОХНЕТ))))) remove unnecessary dependencies and add test controller class

---
+++ src/main/java/ru/shop/PrimeCloud/controllers/TestController.java
@@ -0,0 +1,13 @@
+package ru.shop.PrimeCloud.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+
+    @RequestMapping("/welcome")
+    public String helloWorld() {
+        return "welcome";
+    }
+}
